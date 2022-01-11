11a93bad88fc82115373ab5aa5995704cb36ef67
aniketcodes <aniketsinha1304@gmail.com>
2022-01-11 10:36:58 +0530

    validation for search and filter

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -9,7 +9,7 @@ import {
   Query,
 } from '@nestjs/common';
 import { TasksService } from './tasks.service';
-import { Task, TaskStatus } from './task.model';
+import { Task } from './task.model';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTaskFilterDto } from './dto/get-task-filter.dto';
 import { UpdateTaskStatusDto } from './dto/update-task-status.dto';
